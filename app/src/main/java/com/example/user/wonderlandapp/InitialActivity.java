e82f2b8797362bdafd6016097d84335b426bbba0
zaychik <alexandrsienin@gmail.com>
2017-01-04 17:02:24 +0200

    tabs template working

--- app/src/main/java/com/example/user/wonderlandapp/InitialActivity.java
+++ app/src/main/java/com/example/user/wonderlandapp/InitialActivity.java
@@ -1,7 +1,9 @@
 package com.example.user.wonderlandapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class InitialActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class InitialActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial);
+    }
+
+    public void btn1_onClick(View view) {
+        Intent myIntent = new Intent(this, StateActivity.class);
+        startActivity(myIntent);
     }
 }
